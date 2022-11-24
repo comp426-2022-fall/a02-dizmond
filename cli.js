7cb64dd713eddf061f504d9ad04209ad389a96cf
dizmond <dizmond@live.unc.edu>
2022-11-24 12:37:24 -0500

    galosh linked

--- cli.js
+++ cli.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
-const minimist = require('minimist')
-const nodeFetch = require('node-fetch')
-const momentTimezone = require('moment-timezone')
+import minimist from 'minimist';
+import fetch from 'node-fetch';
+import moment from 'moment-timezone';
+
+const timezone = moment.tz.guess()
+// Make a request
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.91&longitude=-79.06&hourly=temperature_2m');
+
+// Get the data from the request
+console.log(response);
+console.log(timezone);
+
+
+// const days = args.d
